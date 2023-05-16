b2cbaf601edf2718403b89ac30b4eb6a2a9ed304
Cimpean Alexandru <115628859+cimpeanalexandru@users.noreply.github.com>
2023-05-16 19:01:39 +0300

    fix the hour hand not moving hour 12

--- index.js
+++ index.js
@@ -6,50 +6,50 @@ const date = document.querySelector('.date');
 
 
 function setDate() {
-  const now = new Date();
+    const now = new Date();
 
-  //seconds
-  const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    //seconds
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-  //miutes
-  const minutes = now.getMinutes();
-  const minutesDegrees = ((minutes / 60) * 360) + 90;
-  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    //minutes
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
-  //hours
-  const hours = now.getHours();
-  const hoursDegrees = ((hours / 24) * 360) +90; 
-  hourHand.style.transform = `rotate(${hoursDegrees}deh)`;
+    //hours
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
-function showTime(){
+function showTime() {
     var date = new Date();
     var h = date.getHours(); // 0 - 23
     var m = date.getMinutes(); // 0 - 59
     var s = date.getSeconds(); // 0 - 59
     var session = "AM";
-    
-    if(h == 0){
+
+    if (h == 0) {
         h = 12;
     }
-    
-    if(h > 12){
+
+    if (h > 12) {
         h = h - 12;
         session = "PM";
     }
-    
+
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
-    
+
     var time = h + ":" + m + ":" + s + " " + session;
     document.getElementById("MyClockDisplay").innerText = time;
     document.getElementById("MyClockDisplay").textContent = time;
-    
+
     setTimeout(showTime, 1000);
-    
+
 }
 
 setInterval(setDate, 1000);
